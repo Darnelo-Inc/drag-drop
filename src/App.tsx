29be435b61c5fe2cdbaeeeb3376a46bd902b0f43
Pavel Gorbunov <darnelo@inbox.ru>
2023-06-08 20:40:28 +0300

    functions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, MouseEvent } from "react"
+import { ICard } from "./models/card"
+import "./App.css"
 
 function App() {
+  const [cards, setCards] = useState<ICard[]>([
+    { id: 1, order: 3, text: "card-3" },
+    { id: 2, order: 1, text: "card-1" },
+    { id: 3, order: 2, text: "card-2" },
+    { id: 4, order: 4, text: "card-4" },
+  ])
+
+  const [currentCard, setCurrentCard] = useState<ICard | null>(null)
+
+  const dragStartHandler = (e: MouseEvent<HTMLDivElement>, card: ICard) => {
+    console.log("drag", card)
+    setCurrentCard(card)
+  }
+
+  const dragEndHandler = (e: MouseEvent<HTMLDivElement>) => {
+    setCurrentCard(null)
+  }
+
+  const dragOverHandler = (e: MouseEvent<HTMLDivElement>) => {
+    // e.target.style.background("lightgray")
+  }
+
+  const dragLeaveHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+  }
+
+  const dropHandler = (e: MouseEvent<HTMLDivElement>, card: ICard) => {
+    e.preventDefault()
+    console.log("drop", card)
+    setCards([...cards])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {cards.map((card) => (
+        <div
+          key={card.id}
+          className="card"
+          draggable
+          onDragStart={(e) => dragStartHandler(e, card)}
+          onDragEnd={(e) => dragEndHandler(e)}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDragLeave={(e) => dragLeaveHandler(e)}
+          onDrop={(e) => dropHandler(e, card)}
         >
-          Learn React
-        </a>
-      </header>
+          {card.text}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
